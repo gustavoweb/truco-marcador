0ab09ce7aaf6355102272d773cb8230af7c8d1cd
gustavoweb <contato@gustavoweb.com>
2024-01-07 10:04:42 -0300

    front 07/01

--- assets/js/truco.js
+++ assets/js/truco.js
@@ -1,17 +1,51 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var plus = document.querySelector('.plus');
-    var items = plus.children;
+/*document.addEventListener('DOMContentLoaded', function () {
+    var plus = document.querySelector('.plus')
+    var items = plus.children
 
     // Ocultar os itens no início
     for (var i = 0; i < items.length; i++) {
-      items[i].classList.add('hidden');
+      items[i].classList.add('hidden')
     }
 
     plus.addEventListener('click', function () {
-      plus.classList.toggle('plus-collapsed');
+    plus.classList.toggle('plus-collapsed')
+    plus.style.backgroundColor = 'transparent'
 
       for (var i = 0; i < items.length; i++) {
-        items[i].classList.toggle('hidden');
+        items[i].classList.toggle('hidden')
+        // plus.style.backgroundColor = '#0F0'
       }
-    });
-  });
+    })
+  })
+  */
+  document.addEventListener('DOMContentLoaded', function () {
+    var plus = document.querySelector('.plus')
+    // var bt = document.querySelector('.bt')
+    var buttons = plus.querySelectorAll('button')
+
+    // // Oculta os botões inicialmente
+    // for (var i = 0; i < buttons.length; i++) {
+    //   buttons[i].classList.add('hidden');
+    // }
+
+
+    //primeiro click
+    plus.addEventListener('click', function () {
+      plus.classList.toggle('transparent')
+      for (var i = 0; i < buttons.length; i++) {
+        buttons[i].classList.toggle('hidden')
+        // console.log('primeiro click')
+      }
+    })
+
+
+
+    // bt.addEventListener('click', function () {
+    //     plus.classList.toggle('transparent')
+  
+    //     for (var i = 0; i < bt.length; i++) {
+    //       bt[i].classList.remove('hidden')
+    //     }
+    //   })
+
+  })
