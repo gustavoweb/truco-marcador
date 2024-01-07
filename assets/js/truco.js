f1ad0171f9a75a7fbacad97c341d2eb2a6d790d5
gustavoweb <contato@gustavoweb.com>
2024-01-07 11:04:46 -0300

    front 07/01 - 2

--- assets/js/truco.js
+++ assets/js/truco.js
@@ -1,51 +1,45 @@
-/*document.addEventListener('DOMContentLoaded', function () {
-    var plus = document.querySelector('.plus')
-    var items = plus.children
+function incrementScore(scoreId, trophyId, points) {
+  var scoreElement = document.getElementById(scoreId);
+  var trophyElement = document.getElementById(trophyId);
 
-    // Ocultar os itens no início
-    for (var i = 0; i < items.length; i++) {
-      items[i].classList.add('hidden')
-    }
+  var currentScore = parseInt(scoreElement.innerText);
+  scoreElement.innerText = currentScore + points;
 
-    plus.addEventListener('click', function () {
-    plus.classList.toggle('plus-collapsed')
-    plus.style.backgroundColor = 'transparent'
+  if (currentScore + points >= 12) {
+      var currentTrophies = parseInt(trophyElement.innerText.split('=')[1].trim());
+      trophyElement.innerText = '= ' + (currentTrophies + 1);
+      scoreElement.innerText = 0;
+  }
+}
 
-      for (var i = 0; i < items.length; i++) {
-        items[i].classList.toggle('hidden')
-        // plus.style.backgroundColor = '#0F0'
+function incrementScoreWithTrophy(scoreId, trophyId, points) {
+  var scoreElement = document.getElementById(scoreId);
+  var trophyElement = document.getElementById(trophyId);
+
+  var currentTrophies = parseInt(trophyElement.innerText.split('=')[1].trim());
+  trophyElement.innerText = '= ' + (currentTrophies + 1);
+  scoreElement.innerText = 0;
+}
+
+function decrementScore(scoreId, trophyId) {
+  var scoreElement = document.getElementById(scoreId);
+  var trophyElement = document.getElementById(trophyId);
+
+  var currentScore = parseInt(scoreElement.innerText);
+  if (currentScore > 0) {
+      scoreElement.innerText = currentScore - 1;
+  } else {
+      var currentTrophies = parseInt(trophyElement.innerText.split('=')[1].trim());
+      if (currentTrophies > 0) {
+          trophyElement.innerText = '= ' + (currentTrophies - 1);
+          scoreElement.innerText = 12;
       }
-    })
-  })
-  */
-  document.addEventListener('DOMContentLoaded', function () {
-    var plus = document.querySelector('.plus')
-    // var bt = document.querySelector('.bt')
-    var buttons = plus.querySelectorAll('button')
+  }
+}
 
-    // // Oculta os botões inicialmente
-    // for (var i = 0; i < buttons.length; i++) {
-    //   buttons[i].classList.add('hidden');
-    // }
-
-
-    //primeiro click
-    plus.addEventListener('click', function () {
-      plus.classList.toggle('transparent')
-      for (var i = 0; i < buttons.length; i++) {
-        buttons[i].classList.toggle('hidden')
-        // console.log('primeiro click')
-      }
-    })
-
-
-
-    // bt.addEventListener('click', function () {
-    //     plus.classList.toggle('transparent')
-  
-    //     for (var i = 0; i < bt.length; i++) {
-    //       bt[i].classList.remove('hidden')
-    //     }
-    //   })
-
-  })
+function resetScores() {
+  document.getElementById('pn').innerText = 0;
+  document.getElementById('tn').innerText = '= 0';
+  document.getElementById('pe').innerText = 0;
+  document.getElementById('te').innerText = '= 0';
+}
